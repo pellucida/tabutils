f4b5f1cba94b814d74b7b60500c77f4c606995b9
James Sainsbury <toves@sdf.org>
2023-12-15 12:13:43 +1000

    Remove declarations of functions moved elsewhere 	modified:   src/utils/utils.h

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -9,10 +9,9 @@
 
 # include	"errors/errors.h"
 
-void	fatal (char* fmt, ...);
-void	fatal_error (char* msg, int errn);
 void	spaces (FILE* output, size_t n);
 char*	basename (char* s);
+result_t	get_positive (unsigned long* tp, char* str, char** nextp);
 
 enum	{
 	PROGNAME_GET	= 0,
@@ -26,7 +25,5 @@ static	inline	char*	progname () {
 static	inline	char*	progname_set (char* argv0, char* fallback) {
 	return	progname_util (PROGNAME_SET, argv0, fallback);
 }
-
-result_t	get_positive (unsigned long* tp, char* str, char** nextp);
 
 # endif
